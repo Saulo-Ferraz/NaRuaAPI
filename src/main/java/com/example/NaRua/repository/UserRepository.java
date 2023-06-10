4f2d6f2306e1187e303a075b81cdff298ccea981
athospugliesedev <athospugliesedev@gmail.com>
2023-06-10 20:46:12 -0300

    JpaQuery in userRepository

--- src/main/java/com/example/NaRua/repository/UserRepository.java
+++ src/main/java/com/example/NaRua/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.NaRua.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.NaRua.model.UserModel;
@@ -7,7 +8,9 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserModel, Long>{
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+	// Consultas predefinidas
+
 	List<UserModel> findByUsernameAndEmail(String username, String email);
 	List<UserModel> findByUsernameLike(String username);
 	List<UserModel> findByUsernameNotLike(String username);
@@ -20,5 +23,7 @@ public interface UserRepository extends JpaRepository<UserModel, Long>{
 	void deleteByEmail(String email);
 	void deleteById(Long id);
 	boolean existsByEmail(String email);
-	
+
+	@Query("SELECT u FROM UserModel u WHERE u.username LIKE %:keyword% OR u.email LIKE %:keyword%")
+	List<UserModel> findByKeyword(String keyword);
 }
